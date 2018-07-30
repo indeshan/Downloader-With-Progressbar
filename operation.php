c6d92da9993cfcca3f9578c1fc293775c44f15c8
indeshan <inde.shan@gmail.com>
2018-07-31 00:37:53 +0530

    Commiting record fetching, writing into csv and progress checking logic

---
+++ operation.php
@@ -0,0 +1,11 @@
+<?php
+    /**
+    * @author : Shanteshwar Inde
+    * @email  : inde.shan@gmail.com
+    **/
+    
+require_once('model.php');
+
+$obj = new Model();
+$obj->processData();
+?>
---
+++ progress.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+    * @author : Shanteshwar Inde
+    * @email  : inde.shan@gmail.com
+    **/
+    
+session_start();
+usleep(1000000);
+$filename = $_SESSION['file'];
+$row = $_SESSION['row_count'];
+session_write_close();
+$fp = new SplFileObject($filename, 'r');
+$fp->seek(PHP_INT_MAX);
+$percent = round(($fp->key())*100/$row);
+$data = array("percent" => $percent, "file_name" => $filename);
+echo json_encode($data);
+?>
